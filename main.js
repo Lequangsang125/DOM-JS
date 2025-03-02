fb3308ec108bae16ce59008be873fd85f4ffc005
Le Quang Sang <sanglq125@gmail.com>
2025-03-02 23:17:36 +0700

    .

---
+++ main.js
@@ -0,0 +1,30 @@
+
+//document.querySelector() -> trả về 1 phần tử
+//document.querySelectorAll() -> trả về 1 danh sách phần tử
+
+// const result = document.querySelector('.title')
+// console.log(result);
+
+// const result = document.querySelectorAll('.title')
+// console.log(result);
+
+//innerText
+// const h1 = document.querySelector('#heading')
+
+// console.log(h1.innerText);
+
+// const output = document.querySelector('#output')
+// const courses = ['HTML', 'CSS', 'Javascript']
+
+// const result = courses.map((course) =>{
+//     return `<li>${course}</li>`
+// })
+// const html = `<ul>${result.join('\n')}</ul>`
+
+// output.innerHTML = html
+
+//outerHTML
+// const parent = document.querySelector('#parent')
+// console.log(parent.outerHTML);
+
+//textContent
